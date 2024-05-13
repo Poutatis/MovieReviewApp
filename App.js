9b0c0e2456096ce2e26902718f92f47ec6ecbde8
Poutatis <97979895+Poutatis@users.noreply.github.com>
2024-05-13 04:35:33 +0300

    started working on firebase storage

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+import { initializeApp } from "@react-native-firebase/app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { ReviewsProvider } from "./Context/ReviewsContext";
 import MovieDetails from "./Screens/MovieDetails";
@@ -10,6 +11,18 @@ import Search from "./Screens/Search";
 import WelcomeScreen from "./Screens/Welcome";
 
 const Stack = createStackNavigator();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUnL7UdtUF8FFmuvxaR6biAa8ZcHPWAUU",
+  authDomain: "moviereviewapp-84a73.firebaseapp.com",
+  projectId: "moviereviewapp-84a73",
+  storageBucket: "moviereviewapp-84a73.appspot.com",
+  messagingSenderId: "116191162829",
+  appId: "1:116191162829:web:e1b71f6c3e33188c701484",
+  measurementId: "G-BMC4MJSPZQ",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
